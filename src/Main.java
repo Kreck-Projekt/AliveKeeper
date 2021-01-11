3fdfab72613d45056151cc8f624f7af188bc5997
Niko <nikobo2003@gmail.com>
2021-01-11 11:18:03 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,16 @@ public class Main {
     public static void main(String [] args) throws IOException, InterruptedException {
 
         while(true) {
-            pingz(5000);
+            ping(5000);
             Thread.sleep(20000);
         }
     }
 
     public static void ping(int port) throws IOException {
-        Socket socket = new Socket("localhost", port);
-
-        InputStream input = socket.getInputStream();
-        InputStreamReader reader = new InputStreamReader(input);
-
-        int character;
-        StringBuilder data = new StringBuilder();
-
-        while ((character = reader.read()) != -1) data.append((char) character);
-        System.out.println(data);
-    }
-
-    public static void pingz(int port) throws IOException {
         String FromServer;
         String ToServer;
         try {
-            Socket clientSocket = new Socket("localhost", 5000);
-
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
-                System.in));
+            Socket clientSocket = new Socket("localhost", port);
 
         PrintWriter outToServer = new PrintWriter(
                 clientSocket.getOutputStream(), true);
